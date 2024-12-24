53fe8c3ac3a9832fd307ed1824f5227f1935276f
FRJ74 <frankr74@gmail.com>
2024-12-23 18:14:51 -0600

    Return value of mean

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/* Calculate mean logic */
+const getMean = (array) => {
+    const sum = array.reduce((acc,el) => acc + el, 0)
+    const mean = sum/array.length;    
+};
+
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
