bc60c0961a7dc4176eae9460a002c0ea98cb5adb
FRJ74 <frankr74@gmail.com>
2025-01-01 21:03:16 -0600

    Calculate the variance

--- script.js
+++ script.js
@@ -52,4 +52,7 @@ const getMode = (array) => {
 
   const getVariance = array => {
     const mean = getMean(array);
+    const differences = array.map(el => el - mean);
+    const squaredDifferences = differences.map(el => el ** 2);
+    const sumSquaredDifferences = squaredDifferences.reduce((acc, el) => acc + el, 0);
   };
